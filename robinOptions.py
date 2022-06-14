3473df5824134fc97d20c363a8d732c66ee1cf2f
parth <parthib4school@gmail.com>
2022-06-13 17:38:08 -0700

    Third commit.

--- robinOptions.py
+++ robinOptions.py
@@ -26,6 +26,9 @@ start(u, p) # If authentication code is not cached, then a code will be prompted
 def get_key_list(dict):
     return dict.keys()
 
+def get_rounded_amount(amt):
+    return round(float(amt))
+
 def get_greeks(id, greek="delta"): # Valid greek strings are: delta, gamma, theta, rho, vega.
     ret = robin_stocks.robinhood.options.get_option_market_data_by_id(id, greek)
     return ret
@@ -39,8 +42,11 @@ def get_option_instrument_data(id, key=None): # DO STUFF HERE<__----------------
     option_data = robin_stocks.robinhood.options.get_option_instrument_data_by_id(id, key) #Returns the option instrument information.
     return option_data # Valid dict_keys list: (['chain_id', 'chain_symbol', 'created_at', 'expiration_date', 'id', 'issue_date', 'min_ticks', 'rhs_tradability', 'state', 'strike_price', 'tradability', 'type', 'updated_at', 'url', 'sellout_datetime', 'long_strategy_code', 'short_strategy_code'])
 
-def get_rounded_amount(amt):
-    return round(float(amt))
+def get_strike_price(id): #Returns strike price of option id passed in
+    return get_option_instrument_data(id, "strike_price")
+
+# def account_put_or_call(id)  make function to reverse greeks based on if call or put
+
 
 # All of the following variables are lists.
 general_option_info = get_open_option_info()
@@ -59,14 +65,22 @@ amount = list(map(get_rounded_amount, get_open_option_info("quantity")))
 
 print(get_option_instrument_data(option_id[0], "strike_price"))
 
-def get_net_delta(ticker=None): # need to fix this to account for tickers + shares of ticker w/ ticker param... <----------
-    
-    for o in range(len(general_option_info)):
-        delta = get_greeks(option_id[o], "delta") * amount[o] #delta list in string form
-        delta = sum(list(map(float, delta))) #conversion to float and summed to account for quantity
-        print(delta * 100)
+def get_net_delta(ticker=None): # Get portfolio net delta or net delta for a given ticker # need to fix this to account for tickers + shares of ticker w/ ticker param... <----------
+    net_delta = [] # work on this 
+    if ticker == (None):
+        for o in range(len(general_option_info)):
+            delta = get_greeks(option_id[o], "delta") * amount[o] #delta list in string form
+            delta = sum(list(map(float, delta))) * 100 #conversion to float and summed to account for quantity, multiplied by 100 to account for options multiplier
+            print(delta)
+    else:
+        for o in range(len(symbol)):
+            if symbol[o] == ticker:
+                delta = get_greeks(option_id[o], "delta") * amount[o] #delta list in string form
+                delta = sum(list(map(float, delta))) * 100 #conversion to float and summed to account for quantity, multiplied by 100 to account for options multiplier
+                print(delta)
 
 
 
 
-get_net_delta()
+
+get_net_delta("ALLY")
