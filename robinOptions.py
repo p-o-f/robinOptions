23b33bfe1fcefef0cfa52cb79b17bce417b36911
parth <parthib4school@gmail.com>
2022-06-16 04:11:00 -0700

    sleep schedule bad

--- robinOptions.py
+++ robinOptions.py
@@ -32,9 +32,10 @@ def get_rounded_amount(amt):
 def get_open_stock_info(key=None): # Returns a list of stocks that are currently held. 
     return robin_stocks.robinhood.account.get_open_stock_positions(key) 
 #   Valid dict_keys list: (['url', 'instrument', 'instrument_id', 'account', 'account_number', 'average_buy_price', 'pending_average_buy_price', 'quantity', 'intraday_average_buy_price', 'intraday_quantity', 'shares_available_for_exercise', 'shares_held_for_buys', 'shares_held_for_sells', 'shares_held_for_stock_grants', 'shares_held_for_options_collateral', 'shares_held_for_options_events', 'shares_pending_from_options_events', 'shares_available_for_closing_short_position', 'ipo_allocated_quantity', 'ipo_dsp_allocated_quantity', 'avg_cost_affected', 'avg_cost_affected_reason', 'is_primary_account', 'updated_at', 'created_at'])
+
 def get_greeks(id, greek="delta"): #Returns greek value for an option. Valid greek strings are: delta, gamma, theta, rho, vega.
     return robin_stocks.robinhood.options.get_option_market_data_by_id(id, greek)
-
+    
 def get_open_option_info(key=None):
     open_options = robin_stocks.robinhood.options.get_open_option_positions(key) #Returns a list of dictionaries of key/value pairs for each option. If info parameter is provided, a list of strings is returned where the strings are the value of the key that matches info.
 #   Valid dict_keys list: (['account', 'average_price', 'chain_id', 'chain_symbol', 'id', 'option', 'type', 'pending_buy_quantity', 'pending_expired_quantity', 'pending_expiration_quantity', 'pending_exercise_quantity', 'pending_assignment_quantity', 'pending_sell_quantity', 'quantity', 'intraday_quantity', 'intraday_average_open_price', 'created_at', 'trade_value_multiplier', 'updated_at', 'url', 'option_id'])
@@ -58,16 +59,6 @@ amount = list(map(get_rounded_amount, get_open_option_info("quantity")))
 
 general_stock_info = get_open_stock_info()
 stock_id = get_open_stock_info("instrument_id")
-
-"""for o in range(len(general_option_info)):
-    print(symbol[o] + " " + str(amount[o]) + " " + short_or_long[o])
-    delta  = str(get_greeks(option_id[o], "delta") * amount[o])
-    gamma = get_greeks(option_id[o], "gamma") * amount[o]
-    theta = get_greeks(option_id[o], "theta") * amount[o]
-    vega = get_greeks(option_id[o], "vega") * amount[o]
-    print("Delta: " + delta)"""
-
-#print(get_option_instrument_data(option_id[0], "strike_price"))
 
 def get_net_delta(ticker=None): # Get portfolio net delta or net delta for a given ticker. NEED TO FIX TO ACCOUNT FOR DELTA REVERSE IF SHORT + SHARES.
     net_delta = 0 
